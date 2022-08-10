d0c290733c4a0b5c95475c0b10924362fe32e1ad
Athea DeLing <almdeling@gmail.com>
2022-08-10 16:01:02 -0700

    add css add age and height info
    Co-authored-by: Samuel Majerus <ladolego@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,43 @@
+// User Interface Logic
+
+function hideResultsAndError() {
+  document.getElementById("Chris Evans").setAttribute("class", "hidden");
+  document.getElementById("Lily James").setAttribute("class", "hidden");
+  document.getElementById("Hank Green").setAttribute("class", "hidden");
+  document.getElementById("Sorry").setAttribute("class", "hidden");
+}
+
 window.onload = function() {
-  let form = document.querySelector("form");
+  let form = document.querySelector("form#userInfo"); 
   form.onsubmit = function(event) {
     // in this section we get the value for each form input
-    const uAge = document.getElementById("ageInput").value;
-    const uHeight = document.getElementById("heightInput").value;
-    const uPet = document.getElementById("petInput").value;
-/*    const exclamationInput = document.getElementById("exclamationInput").value;
+    const age = parseInt(document.querySelector("input#ageInput").setAttribute("class", "hidden"));
+    const height = parseInt(document.querySelector("input#heightInput").setAttribute("class", "hidden"));
+    //const pet = document.querySelector("input#petInput").setAttribute("class", "hidden");
+/*
+    const exclamationInput = document.getElementById("exclamationInput").value;
     const verbInput = document.getElementById("verbInput").value;
     const nounInput = document.getElementById("nounInput").value;
 */ 
-    // then we set the variables to the values we got from the form
-    document.querySelector("span#person1a").innerText = person1Input;
-    document.querySelector("span#person1b").innerText = person1Input;
-    document.querySelector("span#person1c").innerText = person1Input;
-    document.querySelector("span#person2a").innerText = person2Input;
-    document.querySelector("span#person2b").innerText = person2Input;
-    document.querySelector("span#animal").innerText = animalInput;
-    document.querySelector("span#verb").innerText = verbInput;
-    document.querySelector("span#noun").innerText = nounInput;
-    document.querySelector("span#exclamation").innerText = exclamationInput;
+  
 
-    document.querySelector("div#story").removeAttribute("class");
+    if (age && height) {
+      if (age >= 25 && height >= 64 ) {
+        document.getElementById("Chris Evans").removeAttribute("class"); 
+        document.getElementById("Lily James").removeAttribute("class");
+        document.getElementById("Hank Green").removeAttribute("class");
+      } else if (age >= 25 || height >= 48 ) {
+        document.getElementById("Lily James").removeAttribute("class");
+        document.getElementById("Hank Green").removeAttribute("class");
+      } else if (age >= 23) {
+        document.getElementById("Hank Green").removeAttribute("class");
+      } else {
+        document.getElementById("Sorry").removeAttribute("class");
+      }
 
-    event.preventDefault();
-  };
+      document.getElementById("quiz").removeAttribute("class"); 
+
+      event.preventDefault(); 
+    }
+  }; 
 };
